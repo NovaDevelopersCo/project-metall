c4e2f723e9de8b66b1ec9b0a47289739204ddb9e
Amir <turgunovamir9@gmail.com>
2024-02-28 15:33:35 +0800

    delete unnecessary files

--- src/components/footer/FooterArr.type.ts
+++
@@ -1,9 +0,0 @@
-export interface TFooterArrType {
-	text?: string
-	// href?: string
-	styles?: string
-	// src?: string
-	// alt?: string
-	// width: number
-	// height: number
-}
--- src/components/footer/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Footer'
-export * from './footertop'
-export * from './footericons'
-export * from './footerbottom'
