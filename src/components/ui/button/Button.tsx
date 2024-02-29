dc623dbfaca6a190e9fb7ddbedb4aa3858ea9c2b
Eduard <>
2024-02-29 05:51:46 +0100

    made button component & add css color variables

---
+++ src/components/ui/button/Button.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+
+import document from '@/assets/icons/document.svg'
+import { classNames } from '@/utils/classNames'
+
+import cl from './Button.module.scss'
+
+type ButtonProps = {
+	text: string
+	icon?: boolean
+	width?: number
+	height?: number
+	variant?: 'primary' | 'outline'
+}
+
+export const Button = ({
+	text,
+	icon,
+	variant = 'primary',
+	width = 221,
+	height = 55
+}: ButtonProps) => {
+	return (
+		<button
+			style={{ width: `${width}px`, height: `${height}px` }}
+			className={classNames(cl.root, variant && cl[variant])}>
+			{icon && (
+				<Image
+					alt={text}
+					width={24}
+					height={24}
+					className={cl.root__icon}
+					src={document}
+				/>
+			)}
+			<span className={cl.root__text}>{text}</span>
+		</button>
+	)
+}
