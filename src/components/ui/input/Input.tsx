b8f7376995b1a3703ee7e8daaa324194391a0407
Amir <turgunovamir9@gmail.com>
2024-03-03 19:32:59 +0800

    paul test

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -24,7 +24,18 @@ export const Input = ({
 
 	const [phoneNumber, setPhoneNumber] = useState('+7')
 
-	const handleInputChange = (e: { target: { value: any } }) => {
+	// const handleInputChange = (e: { target: { value: string } }) => {
+	// 	const inputValue = e.target.value.split('')
+
+	// 	if (inputValue[0] === '+' && inputValue[1] === '7') {
+	// 		setPhoneNumber(inputValue.join(''))
+	// 	} else if (inputValue[0] === '+' && inputValue.length === 1) {
+	// 		setPhoneNumber('+7')
+	// 	} else if (inputValue.length >= 3) {
+	// 	}
+	// }
+
+	const handleInputChange = (e: { target: { value: string } }) => {
 		const inputValue = e.target.value
 
 		if (inputValue.startsWith('+7')) {
@@ -32,6 +43,19 @@ export const Input = ({
 		} else if (inputValue.startsWith('+') && inputValue.length === 1) {
 			setPhoneNumber('+7')
 		}
+
+		const cleanedPhoneNumber = inputValue.replace(/\D/g, '')
+		let formattedPhoneNumber = ''
+
+		if (cleanedPhoneNumber.length <= 3) {
+			formattedPhoneNumber = `(${cleanedPhoneNumber})`
+		} else if (cleanedPhoneNumber.length <= 6) {
+			formattedPhoneNumber = `(${cleanedPhoneNumber.slice(0, 3)}) ${cleanedPhoneNumber.slice(3)}`
+		} else {
+			formattedPhoneNumber = `(${cleanedPhoneNumber.slice(0, 3)}) ${cleanedPhoneNumber.slice(3, 6)}-${cleanedPhoneNumber.slice(6, 10)}`
+		}
+
+		setPhoneNumber(formattedPhoneNumber)
 	}
 
 	switch (variant) {
