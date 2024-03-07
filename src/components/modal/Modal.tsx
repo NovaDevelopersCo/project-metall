759da879b7f096e14efe03224d183aa053cd88cf
Amir <turgunovamir9@gmail.com>
2024-03-07 21:52:14 +0800

    make modal

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,81 @@
+import Image from 'next/image'
+import { Dispatch, ReactNode, SetStateAction } from 'react'
+
+import Close from '@/assets/close.svg'
+
+import cl from './Modal.module.scss'
+
+export const Modal = ({
+	active,
+	setActive,
+	children,
+	variant
+}: {
+	active: boolean
+	setActive: Dispatch<SetStateAction<boolean>>
+	children: ReactNode
+	variant: string
+}) => {
+	switch (variant) {
+		case 'default':
+			return (
+				<div
+					className={`${cl.root}`}
+					onClick={() => setActive(false)}>
+					<div
+						className={
+							active
+								? `${cl.root__content} ${cl.root__content__activeContent}`
+								: `${cl.root__content}`
+						}
+						onClick={e => e.stopPropagation()}>
+						{children}
+					</div>
+				</div>
+			)
+		case 'form':
+			return (
+				<div
+					className={
+						active ? `${cl.root} ${cl.root__active}` : `${cl.root}`
+					}
+					onClick={() => setActive(false)}>
+					<div
+						className={
+							active
+								? `${cl.root__content} ${cl.root__content__activeContent}`
+								: `${cl.root__content}`
+						}
+						onClick={e => e.stopPropagation()}>
+						<span
+							className={cl.root__content__close}
+							onClick={() => setActive(false)}>
+							<Image
+								width={16}
+								height={16}
+								src={Close}
+								alt="close"
+							/>
+						</span>
+						<span>{children}</span>
+					</div>
+				</div>
+			)
+	}
+
+	return (
+		<div
+			className={active ? `${cl.root} ${cl.root__active}` : `${cl.root}`}
+			onClick={() => setActive(false)}>
+			<div
+				className={
+					active
+						? `${cl.root__content} ${cl.root__content__activeContent}`
+						: `${cl.root__content}`
+				}
+				onClick={e => e.stopPropagation()}>
+				{children}
+			</div>
+		</div>
+	)
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,35 @@
+'use client'
+
+import { useState } from 'react'
+
+import { Modal } from '@/components/modal/Modal'
+
 export const HomePage = () => {
-	return <></>
+	const [modalActive, setModalActive] = useState(false)
+
+	return (
+		<>
+			<button onClick={() => setModalActive(true)}>open modal</button>
+			<Modal
+				variant="form"
+				active={modalActive}
+				setActive={setModalActive}>
+				<p>text</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Libero, quasi beatae corporis laborum autem exercitationem
+					nulla veritatis harum consequuntur delectus odio accusamus
+					pariatur. Enim dolorem aliquam eum illum voluptatibus
+					blanditiis!
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Libero, quasi beatae corporis laborum autem exercitationem
+					nulla veritatis harum consequuntur delectus odio accusamus
+					pariatur. Enim dolorem aliquam eum illum voluptatibus
+					blanditiis!
+				</p>
+			</Modal>
+		</>
+	)
 }
