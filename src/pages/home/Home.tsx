7a043cbfe5795daf13e8441a0253c4e0e93b699b
Amir <turgunovamir9@gmail.com>
2024-03-09 20:28:13 +0800

    fix

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,43 @@
+'use client'
+
+import { useState } from 'react'
+
+import { Modal } from '@/components/modal/Modal'
+
 export const HomePage = () => {
-	return <></>
+	const [open, setOpen] = useState(false)
+
+	return (
+		<>
+			<button onClick={() => setOpen(true)}>open Modal</button>
+			<Modal
+				active={open}
+				setActive={setOpen}>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+				facilis in mollitia dolores impedit saepe quia modi recusandae.
+				Libero aut quia, rem id accusamus minima minus iste voluptatum
+				voluptates molestias. Lorem ipsum dolor sit amet consectetur
+				adipisicing elit. Laborum facilis in mollitia dolores impedit
+				saepe quia modi recusandae. Libero aut quia, rem id accusamus
+				minima minus iste voluptatum voluptates molestias. Lorem ipsum
+				dolor sit amet consectetur adipisicing elit. Laborum facilis in
+				mollitia dolores impedit saepe quia modi recusandae. Libero aut
+				quia, rem id accusamus minima minus iste voluptatum voluptates
+				molestias. Lorem ipsum dolor sit amet consectetur adipisicing
+				elit. Laborum facilis in mollitia dolores impedit saepe quia
+				modi recusandae. Libero aut quia, rem id accusamus minima minus
+				iste voluptatum voluptates molestias. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Laborum facilis in mollitia
+				dolores impedit saepe quia modi recusandae. Libero aut quia, rem
+				id accusamus minima minus iste voluptatum voluptates molestias.
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+				facilis in mollitia dolores impedit saepe quia modi recusandae.
+				Libero aut quia, rem id accusamus minima minus iste voluptatum
+				voluptates molestias.
+			</Modal>
+		</>
+	)
 }
