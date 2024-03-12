7b2664a81aebd35e8e3607348f580c34ba265264
coldnanee <pmihaylov036@gmail.com>
2024-03-12 22:29:13 +0300

    fix bugs

--- src/components/typography/Text/Text.tsx
+++ src/components/typography/Text/Text.tsx
@@ -1,39 +1,39 @@
-import { classNames } from "@/utils/classNames"
-import styles from './styles.module.scss'
-import { Roboto_Flex } from '@next/font/google'
-const font = Roboto_Flex({
-subsets:['latin']
-})
-const variantMap = {
-    small_text: styles.root__small_text,
-    p20: styles.root_p20,
-    p30: styles.root__p30,
-    p24: styles.root__p24,
+import type { ReactNode } from 'react'
 
+import { classNames } from '@/utils/classNames'
+
+import styles from './index.module.scss'
+
+const variantMap = {
+	small_text: styles.root__small_text,
+	p20: styles.root_p20,
+	p30: styles.root__p30,
+	p24: styles.root__p24
 }
 const colorMap = {
-    white: styles.root__white,
-    black: styles.root__black,
-    yellow: styles.root__yellow,
-} 
-type Tprors = {
-    className:string,
-    text:string,
-    color: 'white'|'black'|'yellow',
-    variant: 'p20'|'p30'|'p24'|'small_text'
+	white: styles.root__white,
+	black: styles.root__black,
+	yellow: styles.root__yellow
 }
-export const Text = (
-    {
-        className,
-        text,
-        color = 'black',
-        variant = 'p20',
-
-    }:Tprors
-) => {
-    return (
-        <p className={classNames(variantMap[variant],colorMap[color], font.className) }>
-                    {text}
-        </p>
-    )
+export const Text = ({
+	className,
+	children,
+	color = 'black',
+	variant = 'p20'
+}: {
+	className?: string
+	children: ReactNode
+	color: 'white' | 'black' | 'yellow'
+	variant: 'p20' | 'p30' | 'p24' | 'small_text'
+}) => {
+	return (
+		<p
+			className={classNames(
+				variantMap[variant],
+				colorMap[color],
+				className ?? ''
+			)}>
+			{children}
+		</p>
+	)
 }
--- src/components/typography/Title/Title.tsx
+++ src/components/typography/Title/Title.tsx
@@ -1,62 +1,76 @@
+import type { ReactNode } from 'react'
 
 import { classNames } from '@/utils/classNames'
-import styles from './styles.module.scss'
-import {Roboto_Flex} from '@next/font/google'
-const font = Roboto_Flex()
 
+import styles from './index.module.scss'
 
-type TProps = {
-    className: string,
-    text: string,
-    variant: 'h1'|'h2'|'h3'|'title',
-    color: 'black'|'white',
-
-}
 const colorMap = {
-    black: styles.root__black,
-    white: styles.root__white,  
-} 
-const variantMap ={
-    h1: styles.root__h1,
-    h2: styles.root__h2,
-    h3: styles.root__h3,
-    title:styles.root__title,
+	black: styles.root__black,
+	white: styles.root__white
+}
+const variantMap = {
+	h1: styles.root__h1,
+	h2: styles.root__h2,
+	h3: styles.root__h3,
+	title: styles.root__title
 }
 export const Title = ({
-    className = '',
-    text,
-    variant = 'h2',
-    color  = 'black',
-
-
-}:TProps) => {
-    
-        {
-            switch(variant) {
-                case 'h1': 
-                return(
-                    <h1 className={classNames(colorMap[color],variantMap[variant], font.className,) }>
-                       {text}
-                    </h1>)
-                case 'h2':
-                    return(
-                        <h2 className={classNames(colorMap[color],variantMap[variant], font.className,) }>
-                           {text}
-                        </h2>)
-                case 'h3':
-                    return(
-                        <h3 className={classNames(colorMap[color],variantMap[variant], font.className,) }>
-                           {text}
-                        </h3>)
-                case 'title':
-                    return (
-                        <h4 className={classNames(colorMap[color],variantMap[variant], font.className,) }>
-                           {text}
-                        </h4>
-                    )
-            }
-
-        }
-        
-    
+	className = '',
+	variant = 'h2',
+	color = 'black',
+	children
+}: {
+	className?: string
+	variant: 'h1' | 'h2' | 'h3' | 'title'
+	color: 'black' | 'white'
+	children: ReactNode
+}) => {
+	{
+		switch (variant) {
+			case 'h1':
+				return (
+					<h1
+						className={classNames(
+							colorMap[color],
+							variantMap[variant],
+							className ?? ''
+						)}>
+						{children}
+					</h1>
+				)
+			case 'h2':
+				return (
+					<h2
+						className={classNames(
+							colorMap[color],
+							variantMap[variant],
+							className ?? ''
+						)}>
+						{children}
+					</h2>
+				)
+			case 'h3':
+				return (
+					<h3
+						className={classNames(
+							colorMap[color],
+							variantMap[variant],
+							className ?? ''
+						)}>
+						{children}
+					</h3>
+				)
+			case 'title':
+				return (
+					<h4
+						className={classNames(
+							colorMap[color],
+							variantMap[variant],
+							className ?? ''
+						)}>
+						{children}
+					</h4>
+				)
+		}
+	}
 }
