9be076ffaf385f71a06fced6c35b651e77d624b8
Amir <turgunovamir9@gmail.com>
2024-03-15 12:57:26 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-'use client'
-
-import { Provider } from 'react-redux'
-
 import { HomePage } from '@/pages/home'
-import { store } from '@/store'
 
 export default function Home() {
-	return (
-		<Provider store={store}>
-			<HomePage />
-		</Provider>
-	)
+	return <HomePage />
 }
