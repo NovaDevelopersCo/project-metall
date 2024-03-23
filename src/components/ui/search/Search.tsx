6e71f04354c6f30d441165d9a6155ddfa196e706
Amir <turgunovamir9@gmail.com>
2024-03-23 13:03:34 +0800

    fix

--- src/components/ui/search/Search.tsx
+++ src/components/ui/search/Search.tsx
@@ -4,9 +4,10 @@ import debounce from 'lodash.debounce'
 
 import Image from 'next/image'
 import { ChangeEvent, useCallback, useEffect, useState } from 'react'
+import { useRef } from 'react'
 
 import Loading from '@/assets/loading.svg'
-import svg from '@/assets/search.svg'
+import SearchIcon from '@/assets/search.svg'
 
 import cl from './Search.module.scss'
 
@@ -14,6 +15,7 @@ export const Search = () => {
 	const [value, setValue] = useState<string>('')
 	const [debouncedValue, setDebouncedValue] = useState<string>('')
 	const [isLoading, setIsLoading] = useState<boolean>(false)
+	const inputRef = useRef(null)
 
 	// eslint-disable-next-line
 	const updateSearchValue = useCallback(
@@ -43,9 +45,16 @@ export const Search = () => {
 		}
 	}, [debouncedValue])
 
+	const handleFocusInput = () => {
+		if (inputRef.current) {
+			inputRef.current.focus()
+		}
+	}
+
 	return (
 		<div className={cl.root}>
 			<input
+				ref={inputRef}
 				value={value}
 				className={cl.root__input}
 				type="search"
@@ -53,11 +62,15 @@ export const Search = () => {
 				onChange={handleChange}
 			/>
 			{!isLoading && (
-				<Image
-					className={cl.root__searchIcon}
-					src={svg}
-					alt={'Search'}
-				/>
+				<button
+					className={cl.root__btn}
+					onClick={handleFocusInput}>
+					<Image
+						className={cl.root__btn__searchIcon}
+						src={SearchIcon}
+						alt={'Search'}
+					/>
+				</button>
 			)}
 			{isLoading && (
 				<Image
